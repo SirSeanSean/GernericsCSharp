8ff2d4898b8b15dd7f5eea0e39320043ea209beb
SirSean <Sean.Rosenthal@Gloanyx.com>
2022-10-22 16:46:04 -0400

    Following Along some Bus Route Tutorial

---
+++ GernericsConsole/GernericsConsole/Bus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GernericsConsole
+{
+    internal class Bus
+    {
+        public int RouteNumber { get; set; }
+        public string StartRoute { get; set; }
+        public string EndRoute { get; set; }
+
+        public Bus(int routeNumber)
+        {
+            this.RouteNumber = routeNumber;
+            this.StartRoute = "MKE";
+            this.EndRoute = "RDU";
+        }
+        public Bus(int routeNumber, string startRoute, string endRoute)
+        {
+            this.RouteNumber = routeNumber;
+            this.StartRoute = startRoute;
+            this.EndRoute = endRoute;
+        }
+        public override string ToString() => $"{RouteNumber}: {StartRoute} -> {EndRoute}";
+    }
+}
---
+++ GernericsConsole/GernericsConsole/ConsoleApp.cs
@@ -0,0 +1,25 @@
+﻿using GernericsConsole;
+
+public class ConsoleApp
+{
+    private static void Main(string[] args)
+    {
+        Bus route1 = new Bus(1);
+        Bus route2 = new Bus(2, "LYH", "ORD");
+
+        Bus[] buses = 
+        {
+            route1,
+            route2,
+            new Bus(3,"University","Morecambe"),
+            new Bus(4, "Lancaster", "Keswick")
+        };
+
+        foreach(Bus route in buses)
+        {
+            Console.WriteLine(route);
+        }
+
+        Console.ReadLine();
+    }
+}
